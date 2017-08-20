cfa03d0d33f7bff194a02b4384101c3a8bf0e8cd
Benjamin Lyon <benjaminlyon@Benjamins-MacBook-Pro.local>
2017-08-19 23:03:12 -0500

    Initial Commit

---
+++ testGit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  testGit
+//
+//  Created by Benjamin Lyon on 8/19/17.
+//  Copyright © 2017 Benjamin Lyon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
